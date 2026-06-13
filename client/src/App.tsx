652787704dd597012ce0c2220d22003e9976f793
蛰伏 <3420058193@qq.com>
2026-06-13 16:13:35 +0800

    feat: wire up full app with AppShell, Header, Sidebar, Canvas, Toolbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import AppShell from './components/AppShell';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import InfiniteCanvas from './components/Canvas/InfiniteCanvas';
+import Toolbar from './components/Toolbar/Toolbar';
+import ZoomModal from './components/ZoomModal';
+
 function App() {
-  return <div>Loading...</div>;
+  return (
+    <AppShell>
+      <Header />
+      <div style={{ display: 'flex', flex: 1, overflow: 'hidden' }}>
+        <Sidebar />
+        <div style={{ flex: 1, position: 'relative', overflow: 'hidden' }}>
+          <InfiniteCanvas />
+        </div>
+        <Toolbar />
+      </div>
+      <ZoomModal />
+    </AppShell>
+  );
 }
 
 export default App;
